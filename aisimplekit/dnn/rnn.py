ee062afd8f4fd6e7ea89dfaf889e8c30e83bb4f3
Said Aitmbarek <saitmbarek@aldebaran-robotics.com>
2019-05-31 23:37:55 +0200

    aisimplekit: full generic RnnTextModel class.

--- aisimplekit/dnn/rnn.py
+++ aisimplekit/dnn/rnn.py
@@ -35,7 +35,10 @@ class RnnModelType(object):
 
 class RnnTextModel(object):
     """ Class for RNN Text Model. """
-    def __init__(self, num_words, cat_cols=[], max_seq_title_description_length=100,
+    def __init__(self, tokenizer_num_words, cat_cols=[],
+                    text_seq_cols=[],
+                    num_cols=[], num_transform_spec={},
+                    max_seq_length=100,
                     embedding_file='../input/fasttest-common-crawl-russian/cc.ru.300.vec',
                     embedding_dim1=300, emb_out_size=10,
                     _prepare_df_handler=None,
@@ -43,6 +46,7 @@ class RnnTextModel(object):
                     dropout_0=0.1, dropout_1=0.1, ndense_0=512, ndense_1=64,
                     final_layer_handler=None,
                     loss_fn=None, metrics_fns=None, learning_rates=(0.009, 0.0045),
+                    optimizer="adam",
                     text_spec={}):
         """
         :param text_spec: Specification of text columns, num_word per col, embeddings..
@@ -63,14 +67,25 @@ class RnnTextModel(object):
         self.loss_fn = loss_fn
         self.metrics_fns = metrics_fns
         self.learning_rates = learning_rates
+        self.optimizer = optimizer
 
         # Inputs and Preprocessing
-        self.max_seq_title_description_length = max_seq_title_description_length
+        self.max_seq_length = max_seq_length
         self.cat_cols = cat_cols
+        self.text_seq_cols = text_seq_cols
+        self.num_cols = num_cols
+        self.num_transform_spec = num_transform_spec
         # num_words: the maximum number of words to keep, based
         #   on word frequency. Only the most common `num_words-1` words will be kept.
-        self.tokenizer = Tokenizer(num_words=num_words)
-        self.vocab_size = -1
+        tokenizers = {}
+        for col in self.text_seq_cols:
+            tokenizers[col] = Tokenizer(num_words=tokenizer_num_words)
+        self.tokenizers = tokenizers
+        # self.tokenizer = Tokenizer(num_words=num_words)
+        vocab_size = {}
+        for col in self.text_seq_cols:
+            vocab_size[col] = -1
+        self.vocab_size = vocab_size
         self._prepare_df_handler = _prepare_df_handler
 
         # Embeddings for categorical
@@ -99,11 +114,12 @@ class RnnTextModel(object):
 
     def _fit_text(self, df, traindex):
         """ """
-        all_text = np.hstack([df.loc[traindex,:]['title_description'].str.lower()])
-        self.tokenizer.fit_on_texts(all_text)
-        self.vocab_size = len(self.tokenizer.word_index)+2
-        del(all_text)
-        gc.collect()
+        for col in self.text_seq_cols:
+            all_text = np.hstack([df.loc[traindex,:][col].str.lower()])
+            self.tokenizers[col].fit_on_texts(all_text)
+            self.vocab_size[col] = len(self.tokenizers[col].word_index)+2
+            del(all_text)
+            gc.collect()
 
     def _encode_categorical(self, df):
         """ """
@@ -115,9 +131,15 @@ class RnnTextModel(object):
 
     def _build_text_sequences(self, df):
         """ """
-        df['seq_title_description'] = self.tokenizer.texts_to_sequences(df['title_description'].str.lower())
-        del(df['title_description'])
-        gc.collect()
+        for col in self.text_seq_cols:
+            df['seq_{}'.format(col)] = (
+                self.tokenizers[col]
+                    .texts_to_sequences(
+                        df[col].str.lower()
+                    )
+            )
+            del(df[col])
+            gc.collect()
         return df
 
     def _preprocess_numerical(self, df):
@@ -128,7 +150,10 @@ class RnnTextModel(object):
         #     df['avg_days_up_user'] = np.log1p(df['avg_days_up_user'])
         #     df['avg_times_up_user'] = np.log1p(df['avg_times_up_user'])
         #     df['n_user_items'] = np.log1p(df['n_user_items'])
-        df['item_seq_number'] = np.log(df['item_seq_number'])
+        for col in self.num_cols:
+            if col in self.num_transform_spec.keys():
+                transf_fn = self.num_transform_spec[col]
+                df[col] = transf_fn(df[col])
         return df
 
     def prepare_df(self, df, traindex):
@@ -140,89 +165,100 @@ class RnnTextModel(object):
         df = self._preprocess_numerical(df)
         return df
 
-    def get_keras_data(self, dataset, max_seq_title_description_length):
+    def get_keras_data(self, dataset, max_seq_length):
         """ """
-        data = {
-            'seq_title_description': pad_sequences(dataset.seq_title_description,
-                                        maxlen=max_seq_title_description_length),
-            'region': np.array(dataset.region),
-            'city': np.array(dataset.city),
-            'category_name': np.array(dataset.category_name),
-            'parent_category_name': np.array(dataset.parent_category_name),
-            'param_1': np.array(dataset.param_1),
-            'param123': np.array(dataset.param123),
-            'image_top_1':np.array(dataset.image_top_1),
-            'price': np.array(dataset[["price"]]),
-            'item_seq_number': np.array(dataset[["item_seq_number"]]),
-            # 'avg_ad_days': np.array(dataset[["avg_days_up_user"]]),
-            # 'avg_ad_times': np.array(dataset[["avg_times_up_user"]]),
-            # 'n_user_items': np.array(dataset[["n_user_items"]])
-        }
+        data = {}
+        for col in self.text_seq_cols:
+            data['seq_{}'.format(col)] = pad_sequences(dataset['seq_{}'.format(col)],
+                                        maxlen=max_seq_length)
+                                        # FIXME: max_seq_title.. is common to all text_seq_cols => should not be !
+
+        # checking that text_seq_cols dont contain any categorical simple cols => exclusive.
+        assert(all([col not in self.cat_cols for col in self.text_seq_cols]))
+
+        # categorical + numerical
+        cols = self.cat_cols + self.num_cols
+        for col in cols:
+            data[col] = np.array(dataset[[col]])
+
         return data
 
-    def build_rnn_model(self, embedding_matrix1):
+    def build_rnn_model(self, embedding_matrixes):
         """ """
-        #Inputs
-        seq_title_description = Input(shape=[self.max_seq_title_description_length],
-                                        name="seq_title_description")
-        region = Input(shape=[1], name="region")
-        city = Input(shape=[1], name="city")
-        category_name = Input(shape=[1], name="category_name")
-        parent_category_name = Input(shape=[1], name="parent_category_name")
-        param_1 = Input(shape=[1], name="param_1")
-        param123 = Input(shape=[1], name="param123")
-        image_code = Input(shape=[1], name="image_top_1")
-        price = Input(shape=[1], name="price")
-        item_seq_number = Input(shape=[1], name='item_seq_number')
-        # avg_ad_days = Input(shape=[1], name="avg_ad_days")
-        # n_user_items = Input(shape=[1], name="n_user_items")
-        # avg_ad_times = Input(shape=[1], name="avg_ad_times")
+        # Inputs
+        # 1) sequential columns
+        arr_inputs_seq = []
+        for col in self.text_seq_cols:
+            in_seq = Input(shape=[self.max_seq_length], # FIXME: shouldnt be common to all text_seq_cols
+                                    name="seq_{}".format(col))
+            arr_inputs_seq.append(in_seq)
 
-        #Embeddings layers
-        if self.vocab_size < 0:
-            self.vocab_size = len(self.tokenizer.word_index)+2
-        vocab_size = self.vocab_size
+        # 2) categorical+numerical columns
+        arr_inputs_cat = []
+        for col in self.cat_cols:
+            in_cat = Input(shape=[1], name=col)
+            arr_inputs_cat.append(in_cat)
 
-        emb_seq_title_description = Embedding(
-            vocab_size, self.embedding_dim1, weights=[embedding_matrix1],
-            trainable=False
-        )(seq_title_description)
+        arr_inputs_num = []
+        for col in self.num_cols:
+            in_num = Input(shape=[1], name=col)
+            arr_inputs_num.append(in_num)
+
+        # Embeddings layers
+        col = self.text_seq_cols[0] # FIXME: why do we arbirarily choose the first column  ?????
+        if self.vocab_size[col] < 0:
+            self.vocab_size[col] = len(self.tokenizers[col].word_index)+2
+        global_vocab_size = self.vocab_size[col] ## FIXME: vocab size is for the 1st seq column only ???
+                                            ## FIXME: Why do we use it in all the embeddings ????
+
+        embs_seqs = []
+        for idx, col in enumerate(self.text_seq_cols):
+            if self.vocab_size[col] < 0:
+                self.vocab_size[col] = len(self.tokenizers[col].word_index)+2
+            vocab_size = self.vocab_size[col] ## FIXME: vocab size is for the 1st seq column only ???
+
+            emb_col = Embedding(
+                vocab_size, self.embedding_dim1, weights=[embedding_matrixes[idx]],
+                trainable=False
+            )(arr_inputs_seq[idx])
+            embs_seqs.append(emb_col)
+
+        # emb_seq_title_description = Embedding(
+        #     vocab_size, self.embedding_dim1, weights=[embedding_matrix1],
+        #     trainable=False
+        # )(seq_title_description)
 
         # For each categorical col, transform to vector of scalars using Embedding.
         emb_out_size = self.emb_out_size # embedding output size default
-        emb_region = Embedding(vocab_size, emb_out_size)(region)
-        emb_city = Embedding(vocab_size, emb_out_size)(city)
-        emb_category_name = Embedding(vocab_size, emb_out_size)(category_name)
-        emb_parent_category_name = Embedding(vocab_size, emb_out_size)(parent_category_name)
-        emb_param_1 = Embedding(vocab_size, emb_out_size)(param_1)
-        emb_param123 = Embedding(vocab_size, emb_out_size)(param123)
-        emb_image_code = Embedding(vocab_size, emb_out_size)(image_code)
+
+        embs_cat = []
+        for idx, col in enumerate(self.cat_cols):
+            emb_col = Embedding(global_vocab_size, emb_out_size)(arr_inputs_cat[idx])
+            embs_cat.append(emb_col)
 
         # GRU Model (or LSTM)
+        rnn_layers = []
         if self.model_type is RnnModelType.GRU:
-            rnn_layer1 = GRU(self.n_units)(emb_seq_title_description)
+            rnn_layers = [
+                GRU(self.n_units)(emb)
+                for emb in embs_seqs
+            ]
         elif self.model_type is RnnModelType.LSTM:
-            rnn_layer1 = LSTM(self.n_units)(emb_seq_title_description)
+            rnn_layers = [
+                LSTM(self.n_units)(emb)
+                for emb in embs_seqs
+            ]
         else:
             raise Exception('[error] Unsupported Model Type:{}'.format(self.model_type))
+
         #main layer
         layers = [
-            rnn_layer1,
-            Flatten()(emb_region),
-            Flatten()(emb_city),
-            Flatten()(emb_category_name),
-            Flatten()(emb_parent_category_name),
-            Flatten()(emb_param_1),
-            Flatten()(emb_param123),
-            Flatten()(emb_image_code),
-            price,
-            item_seq_number,
-            # avg_ad_days,
-            # avg_ad_times,
-            # n_user_items,
-            # avg_ad_times
+            *rnn_layers,
+            *[Flatten()(emb) for emb in embs_cat],
+            *arr_inputs_num,
         ]
         main_l = concatenate(layers)
+
         if self.final_layer_handler is not None:
             # Possibility to override defaut double dense layers with dropout
             main_l = self.final_layer_handler(main_l)
@@ -234,14 +270,10 @@ class RnnTextModel(object):
         output = Dense(1, activation="sigmoid") (main_l)
 
         #model
-        inputs = [
-            seq_title_description, region, city, category_name,
-            parent_category_name, param_1, param123, image_code, price, item_seq_number,
-            # avg_ad_days, avg_ad_times, n_user_items, avg_ad_times
-        ]
+        inputs = arr_inputs_seq + arr_inputs_cat + arr_inputs_num # order matters
 
         model = Model(inputs, output)
-        model.compile(optimizer='adam', loss=self.loss_fn, metrics=self.metrics_fns)
+        model.compile(optimizer=self.optimizer, loss=self.loss_fn, metrics=self.metrics_fns)
         self.model = model
 
     def rmse(self, y, y_pred):
@@ -268,13 +300,17 @@ class RnnTextModel(object):
         """ """
         df = self.prepare_df(df, traindex)
 
-        embedding_matrix1 = embeddings.load_embedding_matrix(
-            self.embedding_file,
-            self.vocab_size,
-            self.embedding_dim1,
-            self.tokenizer
-        )
-        self.build_rnn_model(embedding_matrix1)
+        embedding_matrixes = []
+        for col in self.text_seq_cols:
+            embedding_matrix1 = embeddings.load_embedding_matrix(
+                self.embedding_file,
+                self.vocab_size[col],
+                self.embedding_dim1, # FIXME: same for all text_seq_cols ????
+                self.tokenizers[col]
+            )
+            embedding_matrixes.append(embedding_matrix1)
+
+        self.build_rnn_model(embedding_matrixes)
 
         return df
 
@@ -289,8 +325,8 @@ class RnnTextModel(object):
         )
 
         # Fit the NN Model
-        X_train = self.get_keras_data(X_train, self.max_seq_title_description_length)
-        X_valid = self.get_keras_data(X_valid, self.max_seq_title_description_length)
+        X_train = self.get_keras_data(X_train, self.max_seq_length)
+        X_valid = self.get_keras_data(X_valid, self.max_seq_length)
 
         exp_decay = lambda init, fin, steps: (init/fin)**(1/(steps-1)) - 1
 
@@ -321,7 +357,7 @@ class RnnTextModel(object):
         """ """
         X_test = self.get_keras_data(
             df_test,
-            max_seq_title_description_length=self.max_seq_title_description_length
+            max_seq_length=self.max_seq_length
         )
         preds1 = self.model.predict(X_test, batch_size=self.batch_size, verbose=verbose)
         del(X_test)
